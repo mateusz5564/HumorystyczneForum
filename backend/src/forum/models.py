5acefe831c2b06a1f92d37782c8c48c7c0a952ff
mateusz5564 <mateusz5564@gmail.com>
2019-03-15 12:36:06 +0100

    structure of models

--- backend/src/forum/models.py
+++ backend/src/forum/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=100)
+    status = models.CharField(max_length=30)
+    created = models.DateTimeField(auto_now_add=True)
+
+class Post(models.Model):
+    id_user
+    title
+    text 
+    image   
+    post_state
+    created = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    id_user
+    id_post
+    text 
+    created = models.DateTimeField(auto_now_add=True)
+
+class Post_rating(models.Model):
+    id_post
+    id_user 
+
+class Comment_rating(models.Model):
+    id_user
+    id_comment 
+    id_answer
+
+class Comment_answer(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    id_user
+    id_comment
+    text
